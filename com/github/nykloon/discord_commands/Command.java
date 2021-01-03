ca341ff509c5cca9bb9876aff95c08b67f511925
Nyk76 <nykloon12@gmail.com>
2021-01-03 15:44:22 +0100

    added description parameter + getter

--- com/github/nykloon/discord_commands/Command.java
+++ com/github/nykloon/discord_commands/Command.java
@@ -6,25 +6,31 @@ import java.util.regex.Pattern;
 public class Command {
 
     private final ICommand command;
+    private final String description;
     private final String name;
     private final String[] args;
 
     public Command(String content, String prefix, CommandSettings commandSettings) {
         String[] argsWithoutPrefix = content.replaceFirst(Pattern.quote(prefix), "").split("\\s+");
         this.name = commandSettings.isIgnoreLabelCase() ? argsWithoutPrefix[0].toLowerCase() : argsWithoutPrefix[0];
-        this.command = commandSettings.getCommands().getOrDefault(this.name, null);
+        this.command = commandSettings.getCommands().getOrDefault(this.name, null).getKey();
+        this.description = commandSettings.getCommands().getOrDefault(this.name, null).getValue();
         this.args = Arrays.copyOfRange(argsWithoutPrefix, 1, argsWithoutPrefix.length);
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public ICommand getExecutor() {
-        return command;
+        return this.command;
+    }
+
+    public String getDescription() {
+        return this.description;
     }
 
     public String[] getArgs() {
-        return args;
+        return this.args;
     }
 }
